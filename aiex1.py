6f12bd3cd90c11b6893ace9b7bea11bdc0c50be6
max <zebulin-krizz>
2019-07-21 22:17:08 -0700

    1

---
+++ aiex1.py
@@ -0,0 +1,9 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+path = 'c:\\Python\\data\\ex1data1.txt'
+data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
+data.head()
+data.describe()
+data.plot(kind='scatter', x='Population', y='Profit', figsize=(12,8))
