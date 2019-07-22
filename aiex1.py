8f7e1a9a9d441fb3f79a56d1274e71c4d7a44b1c
max <zebulin-krizz>
2019-07-22 15:37:16 +0530

    2

--- aiex1.py
+++ aiex1.py
@@ -2,6 +2,8 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn import linear_model
+
 path = 'c:\\Python\\data\\ex1data1.txt'
 data = pd.read_csv(path, header=None, names=['Population', 'Profit'])
 data.plot(kind='scatter', x='Population', y='Profit', figsize=(12,8))
@@ -69,4 +71,21 @@ ax.set_xlabel('Iterations')
 ax.set_ylabel('Cost')
 ax.set_title('Error vs Training Epoch')
 
+plt.show()
+
+
+
+
+model = linear_model.LinearRegression()
+model.fit(X, y)
+x = np.array(X[:, 1].A1)
+f = model.predict(X).flatten()
+
+fig, ax = plt.subplots(figsize=(12, 8))
+ax.plot(x, f, 'r', label='Prediction')
+ax.scatter(data.Population, data.Profit, label='Training Data')
+ax.legend(loc=2)
+ax.set_xlabel('Population')
+ax.set_ylabel('Profit')
+ax.set_title('Predicted Profit vs. Population Size')
 plt.show()
